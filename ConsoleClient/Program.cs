b4a18d08a3ac9381bfefbd8c7a9a371be3f7ed31
Luis Harvey Triana Vega <harveytriana@hotmail.com>
2020-11-14 23:43:32 -0500

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,71 @@
+﻿// Sample by: harveytriana@gmail.com
+//
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static HubConnection _connection;
+
+        static void Main()
+        {
+            Console.WriteLine("STRAMING TEST\n");
+            Console.WriteLine("Run as Multiple startup projects");
+            Console.WriteLine("Press Enter key when server is ready");
+            Console.ReadKey();
+            Console.Clear();
+
+            var loggerFactory = LoggerFactory.Create(builder => {
+                builder.AddConsole();
+            });
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+
+            logger.LogInformation("Start Thread");
+
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            Task.Run(() => MainAsync(cancellationTokenSource.Token, logger).Wait());
+
+            Console.WriteLine("\nPress any key to end or cancel the thread ...");
+            Console.ReadKey();
+
+            // cancel the thread if this is not over
+            cancellationTokenSource.Dispose();
+
+            // close signalr client
+            _connection.StopAsync().Wait();
+        }
+
+        static async Task MainAsync(CancellationToken cancellationToken, ILogger logger)
+        {
+            var hubUrl = "http://localhost:8016/streamHub";
+            try {
+                _connection = new HubConnectionBuilder()
+                     .WithUrl(hubUrl)
+                     .Build();
+
+                await _connection.StartAsync();
+
+                // QUESTION:
+                // https://github.com/dotnet/AspNetCore.Docs/issues/20562
+                // await? raise exception: 'IAsyncEnumerable<int>' does not contain a definition for 'GetAwaiter'
+                // 
+                var stream = _connection.StreamAsync<int>("CounterEnumerable", 12, 333, cancellationToken);
+
+                await foreach (var count in stream) {
+                    logger.LogInformation($"Received {count}");
+                }
+                logger.LogInformation("Completed");
+            }
+            catch (Exception exception) {
+                logger.LogError(exception.Message);
+            }
+
+        }
+    }
+}
---
+++ StreamingTest/Hubs/StreamHub.cs
@@ -0,0 +1,119 @@
+﻿//
+//
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+//! BASIC SOURCE
+// https://docs.microsoft.com/en-us/aspnet/core/signalr/streaming?view=aspnetcore-3.1
+
+namespace StreamingTest.Hubs
+{
+    public class StreamHub : Hub
+    {
+        private readonly ILogger<StreamHub> _logger;
+
+        public StreamHub(ILogger<StreamHub> logger)
+        {
+            _logger = logger;
+        }
+
+        #region Server-to-client streaming
+        // hub method becomes a streaming hub method when it returns IAsyncEnumerable<T>, ChannelReader<T>
+        // or async versions
+        //
+        public ChannelReader<int> CounterChannel(
+                   int count,
+                   int delay,
+                   CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Run ChannelReader<int> CounterChannel(count: {count}, delay: {delay})");
+
+            var channel = Channel.CreateUnbounded<int>();
+
+            Exception localException = null;
+
+            Task.Run(async () => {
+                try {
+                    for (var i = 0; i < count; i++) {
+                        // Use the cancellationToken in other APIs that accept cancellation
+                        // tokens so the cancellation can flow down to them.
+                        // cancellationToken.ThrowIfCancellationRequested();
+
+                        // ChannelWriter sends data to the client
+                        await channel.Writer.WriteAsync(i, cancellationToken);
+
+                        await Task.Delay(delay, cancellationToken);
+                    }
+                    channel.Writer.Complete();
+                }
+                catch (Exception exception) {
+                    localException = exception;
+                }
+                finally {
+                    channel.Writer.Complete(localException);
+                }
+            });
+            return channel.Reader;
+        }
+
+        // Second apprach. IAsyncEnumerable<T> ... Server Application using Async Streams
+        //
+        public async IAsyncEnumerable<int> CounterEnumerable(
+            int count,
+            int delay,
+            [EnumeratorCancellation]
+            CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Run IAsyncEnumerable<int> CounterEnumerable(count: {count}, delay: {delay})");
+
+            for (int i = 0; i < count; i++) {
+                // 
+                // Check the cancellation token regularly so that the server will stop
+                // producing items if the client disconnects.
+                cancellationToken.ThrowIfCancellationRequested();
+
+                yield return i; // T instance;
+
+                // Use the cancellationToken in other APIs that accept cancellation
+                // tokens so the cancellation can flow down to them.
+                await Task.Delay(delay, cancellationToken);
+            }
+        }
+        #endregion
+
+        #region Client-to-server streaming
+        // Receiving Streams on the Server
+
+        // first approach, ChannelReader<T>
+        public async Task UploadStreamChannel(ChannelReader<string> stream)
+        {
+            _logger.LogInformation($"Run UploadStreamChannel(ChannelReader stream: {stream})", true);
+
+            while (await stream.WaitToReadAsync()) {
+                while (stream.TryRead(out var item)) {
+                    // do something with the stream item
+                    _logger.LogInformation($"From client: {item}", true);
+                }
+            }
+        }
+
+        // Second apporach. IAsyncEnumerable<T>
+        //
+        public async Task UploadStreamEnumerable(IAsyncEnumerable<string> stream)
+        {
+            _logger.LogInformation($"UploadStreamEnumerable(IAsyncEnumerable stream: {stream})", true);
+
+            await foreach (var item in stream) {
+                // do something with the stream item
+                _logger.LogInformation($"From client: {item}", true);
+            }
+        }
+        #endregion
+    }
+}
